18bbc5e6bf37e0078178e53da5a0c9f27e189251
hamza2991 <117831653+hamza2991@users.noreply.github.com>
2023-01-18 13:00:46 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from gettext import install
+from flask import Flask, render_template, request, jsonify
+import numpy as np  
+import joblib
+import util
+import json
+
+
+model= joblib.load("house_model")
+
+
+with open(r'C:\Users\Malik\BHP\columns.json' , 'r') as f:
+     location = json.load(f)['location']
+   
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def welecome():
+    return render_template('index.html', loc= location)
+
+@app.route("/submit", methods= ['POST'])
+def submit():
+    area = float(request.form['area'])
+    bhk= float(request.form['bhk'])
+    loc_index = location.index (request.form['location'])
+    arr = np.array ([[loc_index, area, bhk]])
+    return "price is: " +  str (model.predict(arr))
+
+if __name__=='__main__' :
+    app.run(debug=True)
+
